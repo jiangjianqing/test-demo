84d777c234299d71e2dee703b25be4ada4fd82fa
cz_jjq <cz_jjq@promote.cache-dns.local>
2016-06-18 17:11:25 +0800

    add uicomponent test suite

--- robot-test/var.py
+++ robot-test/var.py
@@ -9,5 +9,5 @@ searchText=u'robot framework 学习'
 LIST_vars=['hello','list','word']
 integer=random.randint(1,10)
 
-host='localhost:8181'
-site='webui.springdm.springsecurity'
+host='192.168.211.130:8181'
+site='webui.springdm.springsecurity/rest'
